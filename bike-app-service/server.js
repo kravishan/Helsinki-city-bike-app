dd55f60126b1f926a10a18113bfa4706c668eb00
Kavindu Ravishan <ravishankavindu@gmail.com>
2023-05-22 20:40:32 +0300

    fixed backend errors

--- bike-app-service/server.js
+++ bike-app-service/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
-const journeyRoutes = require('./journeyRoutes');
+const Routes = require('./routes');
 
 app.use(express.json()); // Enable JSON body parsing
 
-app.use('/api', journeyRoutes);
+app.use('/api', Routes);
 
 app.listen(3001, () => {
   console.log('Backend server is running on port 3001');
